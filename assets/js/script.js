8bcfa1e999c4628aa844b4e4828d9f5447e5c2ac
Guilherme de Aguiar <gpacianotto@gmail.com>
2020-06-19 19:31:45 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+function start() {
+
+    let multi = [];
+    resultados = document.getElementsByClassName('result');
+
+    console.log("jooj");
+    
+}
+
+function mult(a,b){
+    return a*b;
+}
